dff034d5dea3f323750628deb17bbfe8c38f5f7d
Sondre Bye Humberset <sondre@apt.no>
2020-05-05 11:06:43 +0200

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public/assets/js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  }
+};
